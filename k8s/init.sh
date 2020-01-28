755f586cf10dd49f9e05f4911ba1f196b0fc53df
forsrc <forsrc@gmail.com>
2020-01-28 13:44:29 +0900

    Update init.sh

--- k8s/init.sh
+++ k8s/init.sh
@@ -75,6 +75,14 @@ docker exec k8s-master rm -f /etc/cni/net.d/*flannel*
 docker exec k8s-node1  rm -f /etc/cni/net.d/*flannel*
 docker exec k8s-node2  rm -f /etc/cni/net.d/*flannel*
 
+docker exec k8s-master ip link delete cni0
+docker exec k8s-master ip link delete flannel.1
+docker exec k8s-node1  ip link delete cni0
+docker exec k8s-node1  ip link delete flannel.1
+docker exec k8s-node2  ip link delete cni0
+docker exec k8s-node2  ip link delete flannel.1
+
+
 SUBNET_ENV="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
 
 docker exec k8s-master sh -c "$SUBNET_ENV"
