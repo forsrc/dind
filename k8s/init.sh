3cfc0f86ab8385893219141caa26078d2f73b32e
forsrc <forsrc@gmail.com>
2020-01-28 13:31:02 +0900

    Update init.sh

--- k8s/init.sh
+++ k8s/init.sh
@@ -41,9 +41,6 @@ docker run -d -it --privileged=true -d -it \
     -v /dind-k8s/master/var/lib/kubelet/:/var/lib/kubelet/ \
     -v /dind-k8s/master/etc/docker/:/etc/docker/ \
     -v /dind-k8s/master/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/master/dind-k8s:/dind-k8s/ \
-    -p 30080:30080 -p 30443:30443 -p 8080:8080 \
-    -p 30000:30000 -p 30001:30001 -p 30002:30002 -p 30003:30003 -p 30004:30004 -p 30005:30005 \
     --hostname k8s-master \
     --name k8s-master \
     forsrc/dind:k8s /usr/sbin/init
@@ -57,7 +54,6 @@ docker run -d -it --privileged=true -d -it \
     -v /dind-k8s/node1/var/lib/kubelet/:/var/lib/kubelet/ \
     -v /dind-k8s/node1/etc/docker/:/etc/docker/ \
     -v /dind-k8s/node1/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/node1/dind-k8s:/dind-k8s/ \
     --hostname k8s-node1 \
     --name k8s-node1 \
     forsrc/dind:k8s /usr/sbin/init
@@ -71,7 +67,6 @@ docker run -d -it --privileged=true -d -it \
     -v /dind-k8s/node2/var/lib/kubelet/:/var/lib/kubelet/ \
     -v /dind-k8s/node2/etc/docker/:/etc/docker/ \
     -v /dind-k8s/node2/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/node2/dind-k8s:/dind-k8s/ \
     --hostname k8s-node2 \
     --name k8s-node2 \
     forsrc/dind:k8s /usr/sbin/init
@@ -97,7 +92,7 @@ docker exec k8s-node2  sh -c "`docker exec k8s-master kubeadm token create --pri
 
 docker exec k8s-master kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-docker exec k8s-master yum install -y bash-completion
+#docker exec k8s-master yum install -y bash-completion
 docker exec k8s-master sh -c "echo 'source /usr/share/bash-completion/bash_completion' >>~/.bashrc"
 docker exec k8s-master sh -c "echo 'source <(kubectl completion bash)'                 >>~/.bashrc"
 
