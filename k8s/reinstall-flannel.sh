dd167bc1fa0d11eaffe3238cc7d6a5f9081a245e
forsrc <forsrc@gmail.com>
2020-05-22 23:12:25 +0900

    Update reinstall-flannel.sh

--- k8s/reinstall-flannel.sh
+++ k8s/reinstall-flannel.sh
@@ -1,6 +1,6 @@
 echo 262144 > /sys/module/nf_conntrack/parameters/hashsize
   
-kubectl delete -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+docker exec k8s-master sh -c "kubectl delete -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
 sleep 3
 
 SUBNET_ENV_1="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
@@ -11,4 +11,4 @@ docker exec k8s-node1  sh -c "$SUBNET_ENV_2"
 docker exec k8s-node2  sh -c "$SUBNET_ENV_3"
 
 
-kubectl apply  -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+docker exec k8s-master sh -c "kubectl apply  -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
