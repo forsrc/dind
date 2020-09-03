8bed1c83bc815e194ee3a273de48db18a03135d4
forsrc <forsrc@gmail.com>
2020-09-03 23:16:27 +0900

    Update init_with_ssh.sh

--- k8s/init_with_ssh.sh
+++ k8s/init_with_ssh.sh
@@ -1,17 +1,20 @@
+ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa root@k8s-node1 echo ssh ok
+ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa root@k8s-node2 echo ssh ok
+
+sleep 2
 
 exec_all() {
-    echo $1 | sh
-    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node1  sh --
-    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node2  sh --
+    echo $1 | sh --
+    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node1 sh
+    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node2 sh
+     
 }
 
 exec_node() {
-    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node1  sh --
-    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node2  sh --
+    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node1 sh
+    echo $1 | ssh -i ~/.ssh/id_rsa root@k8s-node2 sh
 }
 
-ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa root@k8s-node1 echo ssh ok
-ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa root@k8s-node2 echo ssh ok
 
 exec_all "sed -i -e 's@^ExecStart=/usr/bin/kubelet.*@ExecStart=/usr/bin/kubelet \$KUBELET_KUBECONFIG_ARGS \$KUBELET_CONFIG_ARGS \$KUBELET_KUBEADM_ARGS \$KUBELET_EXTRA_ARGS --fail-swap-on=false@g' /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf"
 exec_all "systemctl daemon-reload && systemctl restart kubelet"
@@ -22,7 +25,7 @@ exec_all "echo 172.7.0.11 k8s-node1  >> /etc/hosts"
 exec_all "echo 172.7.0.12 k8s-node2  >> /etc/hosts"
 
 
-kubeadm init --kubernetes-version=\`kubeadm version -o short\` --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.7.0.10 --ignore-preflight-errors=all
+kubeadm init --kubernetes-version=`kubeadm version -o short` --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.7.0.10 --ignore-preflight-errors=all
 mkdir -p /root/.kube
 cp -f /etc/kubernetes/admin.conf /root/.kube/config
 chown 0:0 /root/.kube/config
