67cd103be8c4cd90b6fa3dcd9fbc0ea58a39d682
forsrc <forsrc@gmail.com>
2024-04-25 08:57:16 +0900

    Update init.sh

--- k8s/init.sh
+++ k8s/init.sh
@@ -53,7 +53,7 @@ docker run -d -it --privileged=true -d -it \
 
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
-    --ip 172.7.0.11 \
+    --ip 172.7.0.20 \
     -v $K8S_HOME/temp:/temp/ \
     -v $K8S_HOME/etc/resolv.conf:/etc/resolv.conf \
     -v $K8S_HOME/node1/var/lib/docker/:/var/lib/docker/ \
@@ -68,7 +68,7 @@ docker run -d -it --privileged=true -d -it \
   
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
-    --ip 172.7.0.12 \
+    --ip 172.7.0.30 \
     -v $K8S_HOME/temp:/temp/ \
     -v $K8S_HOME/etc/resolv.conf:/etc/resolv.conf \
     -v $K8S_HOME/node2/var/lib/docker/:/var/lib/docker/ \
@@ -87,18 +87,18 @@ docker run -d -it --privileged=true -d -it \
 
 docker exec k8s-master sh -c "echo 127.0.0.1  localhost  >  /etc/hosts"
 docker exec k8s-master sh -c "echo 172.7.0.10 k8s-master >> /etc/hosts"
-docker exec k8s-master sh -c "echo 172.7.0.11 k8s-node1  >> /etc/hosts"
-docker exec k8s-master sh -c "echo 172.7.0.12 k8s-node2  >> /etc/hosts"
+docker exec k8s-master sh -c "echo 172.7.0.20 k8s-node1  >> /etc/hosts"
+docker exec k8s-master sh -c "echo 172.7.0.30 k8s-node2  >> /etc/hosts"
 
 docker exec k8s-node1  sh -c "echo 127.0.0.1  localhost  >  /etc/hosts"
 docker exec k8s-node1  sh -c "echo 172.7.0.10 k8s-master >> /etc/hosts"
-docker exec k8s-node1  sh -c "echo 172.7.0.11 k8s-node1  >> /etc/hosts"
-docker exec k8s-node1  sh -c "echo 172.7.0.12 k8s-node2  >> /etc/hosts"
+docker exec k8s-node1  sh -c "echo 172.7.0.20 k8s-node1  >> /etc/hosts"
+docker exec k8s-node1  sh -c "echo 172.7.0.30 k8s-node2  >> /etc/hosts"
 
 docker exec k8s-node2  sh -c "echo 127.0.0.1  localhost  >  /etc/hosts"
 docker exec k8s-node2  sh -c "echo 172.7.0.10 k8s-master >> /etc/hosts"
-docker exec k8s-node2  sh -c "echo 172.7.0.11 k8s-node1  >> /etc/hosts"
-docker exec k8s-node2  sh -c "echo 172.7.0.12 k8s-node2  >> /etc/hosts"
+docker exec k8s-node2  sh -c "echo 172.7.0.20 k8s-node1  >> /etc/hosts"
+docker exec k8s-node2  sh -c "echo 172.7.0.30 k8s-node2  >> /etc/hosts"
 
 #docker exec k8s-master rm -f /etc/cni/net.d/*flannel*
 #docker exec k8s-node1  rm -f /etc/cni/net.d/*flannel*
@@ -112,17 +112,17 @@ docker exec k8s-node2  sh -c "echo 172.7.0.12 k8s-node2  >> /etc/hosts"
 #docker exec k8s-node2  ip link delete flannel.1
 
 
-SUBNET_ENV_1="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
-SUBNET_ENV_2="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.2/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
-SUBNET_ENV_3="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.3/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
+SUBNET_ENV_1="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.10/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
+SUBNET_ENV_2="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.20/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
+SUBNET_ENV_3="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.30/24 >> /run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /run/flannel/subnet.env"
 
 #docker exec k8s-master sh -c "$SUBNET_ENV_1"
 #docker exec k8s-node1  sh -c "$SUBNET_ENV_2"
 #docker exec k8s-node2  sh -c "$SUBNET_ENV_3"
 
-SUBNET_ENV_1="mkdir -p $K8S_HOME/master/run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> $K8S_HOME/master/run/flannel/subnet.env"
-SUBNET_ENV_2="mkdir -p $K8S_HOME/node1/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.2/24 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node1/run/flannel/subnet.env"
-SUBNET_ENV_3="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.3/24 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node2/run/flannel/subnet.env"
+SUBNET_ENV_1="mkdir -p $K8S_HOME/master/run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.10/24 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> $K8S_HOME/master/run/flannel/subnet.env"
+SUBNET_ENV_2="mkdir -p $K8S_HOME/node1/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.20/24 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node1/run/flannel/subnet.env"
+SUBNET_ENV_3="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.30/24 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node2/run/flannel/subnet.env"
 
 sudo sh -c "$SUBNET_ENV_1"
 sudo sh -c "$SUBNET_ENV_2"
