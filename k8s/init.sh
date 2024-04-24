9d07133da7e69a8831100efd808ab93abc6a7c54
forsrc <forsrc@gmail.com>
2024-04-24 21:32:21 +0900

    Update init.sh

--- k8s/init.sh
+++ k8s/init.sh
@@ -5,30 +5,32 @@
 
 #echo -e '{\n\t "dns": ["8.8.8.8"]\n}' > /etc/docker/daemon.json && systemctl daemon-reload && systemctl restart docker
 
-mkdir -p /dind-k8s/temp/
-mkdir -p /dind-k8s/master/var/lib/docker
-mkdir -p /dind-k8s/master/var/lib/kubelet
-mkdir -p /dind-k8s/master/etc/docker
-mkdir -p /dind-k8s/master/etc/kubernetes
-mkdir -p /dind-k8s/node1/var/lib/docker
-mkdir -p /dind-k8s/node1/var/lib/kubelet
-mkdir -p /dind-k8s/node1/etc/docker
-mkdir -p /dind-k8s/node1/etc/kubernetes
-mkdir -p /dind-k8s/node2/var/lib/docker
-mkdir -p /dind-k8s/node2/var/lib/kubelet
-mkdir -p /dind-k8s/node2/etc/docker
-mkdir -p /dind-k8s/node2/etc/kubernetes
+export K8S_HOME=/dind-k8
 
-#rm -rf /dind-k8s/master/etc/kubernetes/*
-#rm -rf /dind-k8s/node1/etc/kubernetes/*
-#rm -rf /dind-k8s/node2/etc/kubernetes/*
+mkdir -p $K8S_HOME/temp/
+mkdir -p $K8S_HOME/master/var/lib/docker
+mkdir -p $K8S_HOME/master/var/lib/kubelet
+mkdir -p $K8S_HOME/master/etc/docker
+mkdir -p $K8S_HOME/master/etc/kubernetes
+mkdir -p $K8S_HOME/node1/var/lib/docker
+mkdir -p $K8S_HOME/node1/var/lib/kubelet
+mkdir -p $K8S_HOME/node1/etc/docker
+mkdir -p $K8S_HOME/node1/etc/kubernetes
+mkdir -p $K8S_HOME/node2/var/lib/docker
+mkdir -p $K8S_HOME/node2/var/lib/kubelet
+mkdir -p $K8S_HOME/node2/etc/docker
+mkdir -p $K8S_HOME/node2/etc/kubernetes
 
-#rm -rf /dind-k8s/master/var/lib/kubelet/*
-#rm -rf /dind-k8s/node1/var/lib/kubelet/*
-#rm -rf /dind-k8s/node2/var/lib/kubelet/*
+#rm -rf $K8S_HOME/master/etc/kubernetes/*
+#rm -rf $K8S_HOME/node1/etc/kubernetes/*
+#rm -rf $K8S_HOME/node2/etc/kubernetes/*
 
-mkdir -p /dind-k8s/etc/
-echo 'nameserver 8.8.8.8' > /dind-k8s/etc/resolv.conf
+#rm -rf $K8S_HOME/master/var/lib/kubelet/*
+#rm -rf $K8S_HOME/node1/var/lib/kubelet/*
+#rm -rf $K8S_HOME/node2/var/lib/kubelet/*
+
+mkdir -p $K8S_HOME/etc/
+echo 'nameserver 8.8.8.8' > $K8S_HOME/etc/resolv.conf
 
 
 docker network create --subnet=172.7.0.0/24 --gateway=172.7.0.1 net-dind-k8s
@@ -36,13 +38,13 @@ docker network create --subnet=172.7.0.0/24 --gateway=172.7.0.1 net-dind-k8s
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
     --ip 172.7.0.10 \
-    -v /dind-k8s/temp:/temp/ \
-    -v /dind-k8s/etc/resolv.conf:/etc/resolv.conf \
-    -v /dind-k8s/master/var/lib/docker/:/var/lib/docker/ \
-    -v /dind-k8s/master/var/lib/kubelet/:/var/lib/kubelet/ \
-    -v /dind-k8s/master/etc/docker/:/etc/docker/ \
-    -v /dind-k8s/master/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/master/run/flannel/:/run/flannel/ \
+    -v $K8S_HOME/temp:/temp/ \
+    -v $K8S_HOME/etc/resolv.conf:/etc/resolv.conf \
+    -v $K8S_HOME/master/var/lib/docker/:/var/lib/docker/ \
+    -v $K8S_HOME/master/var/lib/kubelet/:/var/lib/kubelet/ \
+    -v $K8S_HOME/master/etc/docker/:/etc/docker/ \
+    -v $K8S_HOME/master/etc/kubernetes/:/etc/kubernetes/ \
+    -v $K8S_HOME/master/run/flannel/:/run/flannel/ \
     --hostname k8s-master \
     --name k8s-master \
     forsrc/dind:k8s /usr/sbin/init
@@ -50,13 +52,13 @@ docker run -d -it --privileged=true -d -it \
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
     --ip 172.7.0.11 \
-    -v /dind-k8s/temp:/temp/ \
-    -v /dind-k8s/etc/resolv.conf:/etc/resolv.conf \
-    -v /dind-k8s/node1/var/lib/docker/:/var/lib/docker/ \
-    -v /dind-k8s/node1/var/lib/kubelet/:/var/lib/kubelet/ \
-    -v /dind-k8s/node1/etc/docker/:/etc/docker/ \
-    -v /dind-k8s/node1/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/node1/run/flannel/:/run/flannel/ \
+    -v $K8S_HOME/temp:/temp/ \
+    -v $K8S_HOME/etc/resolv.conf:/etc/resolv.conf \
+    -v $K8S_HOME/node1/var/lib/docker/:/var/lib/docker/ \
+    -v $K8S_HOME/node1/var/lib/kubelet/:/var/lib/kubelet/ \
+    -v $K8S_HOME/node1/etc/docker/:/etc/docker/ \
+    -v $K8S_HOME/node1/etc/kubernetes/:/etc/kubernetes/ \
+    -v $K8S_HOME/node1/run/flannel/:/run/flannel/ \
     --hostname k8s-node1 \
     --name k8s-node1 \
     forsrc/dind:k8s /usr/sbin/init
@@ -64,13 +66,13 @@ docker run -d -it --privileged=true -d -it \
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
     --ip 172.7.0.12 \
-    -v /dind-k8s/temp:/temp/ \
-    -v /dind-k8s/etc/resolv.conf:/etc/resolv.conf \
-    -v /dind-k8s/node2/var/lib/docker/:/var/lib/docker/ \
-    -v /dind-k8s/node2/var/lib/kubelet/:/var/lib/kubelet/ \
-    -v /dind-k8s/node2/etc/docker/:/etc/docker/ \
-    -v /dind-k8s/node2/etc/kubernetes/:/etc/kubernetes/ \
-    -v /dind-k8s/node2/run/flannel/:/run/flannel/ \
+    -v $K8S_HOME/temp:/temp/ \
+    -v $K8S_HOME/etc/resolv.conf:/etc/resolv.conf \
+    -v $K8S_HOME/node2/var/lib/docker/:/var/lib/docker/ \
+    -v $K8S_HOME/node2/var/lib/kubelet/:/var/lib/kubelet/ \
+    -v $K8S_HOME/node2/etc/docker/:/etc/docker/ \
+    -v $K8S_HOME/node2/etc/kubernetes/:/etc/kubernetes/ \
+    -v $K8S_HOME/node2/run/flannel/:/run/flannel/ \
     --hostname k8s-node2 \
     --name k8s-node2 \
     forsrc/dind:k8s /usr/sbin/init
@@ -114,9 +116,9 @@ SUBNET_ENV_3="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /ru
 #docker exec k8s-node1  sh -c "$SUBNET_ENV_2"
 #docker exec k8s-node2  sh -c "$SUBNET_ENV_3"
 
-SUBNET_ENV_1="mkdir -p /dind-k8s/master/run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /dind-k8s/master/run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> /dind-k8s/master/run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> /dind-k8s/master/run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> /dind-k8s/master/run/flannel/subnet.env"
-SUBNET_ENV_2="mkdir -p /dind-k8s/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > /dind-k8s/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.2/24 >> /dind-k8s/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> /dind-k8s/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> /dind-k8s/node1/run/flannel/subnet.env"
-SUBNET_ENV_3="mkdir -p /dind-k8s/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > /dind-k8s/node2/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.3/24 >> /dind-k8s/node2/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> /dind-k8s/node2/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> /dind-k8s/node2/run/flannel/subnet.env"
+SUBNET_ENV_1="mkdir -p $K8S_HOME/master/run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> $K8S_HOME/master/run/flannel/subnet.env"
+SUBNET_ENV_2="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.2/24 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node1/run/flannel/subnet.env"
+SUBNET_ENV_3="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.3/24 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node2/run/flannel/subnet.env"
 
 sh -c "$SUBNET_ENV_1"
 sh -c "$SUBNET_ENV_2"
