d02e54638a5042f4d9f410258814bb9f88ae0567
forsrc <forsrc@gmail.com>
2024-04-25 08:51:25 +0900

    Update init.sh

--- k8s/init.sh
+++ k8s/init.sh
@@ -5,7 +5,8 @@
 
 #echo -e '{\n\t "dns": ["8.8.8.8"]\n}' > /etc/docker/daemon.json && systemctl daemon-reload && systemctl restart docker
 
-export K8S_HOME=/dind-k8
+export K8S_HOME=/dind-k8s
+#export K8S_HOME=/data/docker/k8s
 
 mkdir -p $K8S_HOME/temp/
 mkdir -p $K8S_HOME/master/var/lib/docker
@@ -45,9 +46,10 @@ docker run -d -it --privileged=true -d -it \
     -v $K8S_HOME/master/etc/docker/:/etc/docker/ \
     -v $K8S_HOME/master/etc/kubernetes/:/etc/kubernetes/ \
     -v $K8S_HOME/master/run/flannel/:/run/flannel/ \
+    -v /opt:/opt \
     --hostname k8s-master \
     --name k8s-master \
-    forsrc/dind:k8s /usr/sbin/init
+    forsrc/dind:k8s-jetson /usr/sbin/init
 
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
@@ -61,7 +63,8 @@ docker run -d -it --privileged=true -d -it \
     -v $K8S_HOME/node1/run/flannel/:/run/flannel/ \
     --hostname k8s-node1 \
     --name k8s-node1 \
-    forsrc/dind:k8s /usr/sbin/init
+    -v /opt:/opt \
+    forsrc/dind:k8s-jetson /usr/sbin/init
   
 docker run -d -it --privileged=true -d -it \
     --network net-dind-k8s \
@@ -73,9 +76,10 @@ docker run -d -it --privileged=true -d -it \
     -v $K8S_HOME/node2/etc/docker/:/etc/docker/ \
     -v $K8S_HOME/node2/etc/kubernetes/:/etc/kubernetes/ \
     -v $K8S_HOME/node2/run/flannel/:/run/flannel/ \
+    -v /opt:/opt \
     --hostname k8s-node2 \
     --name k8s-node2 \
-    forsrc/dind:k8s /usr/sbin/init
+    forsrc/dind:k8s-jetson /usr/sbin/init
 
 #docker exec k8s-master sh -c "echo -e '{\n\t \"dns\": [\"8.8.8.8\"]\n}' > /etc/docker/daemon.json && systemctl daemon-reload && systemctl restart docker"
 #docker exec k8s-node1  sh -c "echo -e '{\n\t \"dns\": [\"8.8.8.8\"]\n}' > /etc/docker/daemon.json && systemctl daemon-reload && systemctl restart docker"
@@ -117,12 +121,38 @@ SUBNET_ENV_3="mkdir -p /run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > /ru
 #docker exec k8s-node2  sh -c "$SUBNET_ENV_3"
 
 SUBNET_ENV_1="mkdir -p $K8S_HOME/master/run/flannel/ && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_SUBNET=10.244.0.1/24 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_MTU=1450 >> $K8S_HOME/master/run/flannel/subnet.env && echo FLANNEL_IPMASQ=true >> $K8S_HOME/master/run/flannel/subnet.env"
-SUBNET_ENV_2="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.2/24 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node1/run/flannel/subnet.env"
+SUBNET_ENV_2="mkdir -p $K8S_HOME/node1/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.2/24 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node1/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node1/run/flannel/subnet.env"
 SUBNET_ENV_3="mkdir -p $K8S_HOME/node2/run/flannel/  && echo FLANNEL_NETWORK=10.244.0.0/16 > $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_SUBNET=10.244.0.3/24 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_MTU=1450 >> $K8S_HOME/node2/run/flannel/subnet.env  && echo FLANNEL_IPMASQ=true >> $K8S_HOME/node2/run/flannel/subnet.env"
 
-sh -c "$SUBNET_ENV_1"
-sh -c "$SUBNET_ENV_2"
-sh -c "$SUBNET_ENV_3"
+sudo sh -c "$SUBNET_ENV_1"
+sudo sh -c "$SUBNET_ENV_2"
+sudo sh -c "$SUBNET_ENV_3"
+
+
+cat  <<EOF > $K8S_HOME/master/etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+  },
+  "storage-driver": "overlay2",
+  "default-runtime": "nvidia"
+}
+EOF
+
+cp -f $K8S_HOME/master/etc/docker/daemon.json $K8S_HOME/node1/etc/docker/daemon.json
+cp -f $K8S_HOME/master/etc/docker/daemon.json $K8S_HOME/node2/etc/docker/daemon.json
+
+docker exec k8s-master sh -c "rm -rf /etc/containerd/config.toml &&  systemctl restart containerd"
+docker exec k8s-node1  sh -c "rm -rf /etc/containerd/config.toml &&  systemctl restart containerd"
+docker exec k8s-node2  sh -c "rm -rf /etc/containerd/config.toml &&  systemctl restart containerd"
 
 docker exec k8s-master sh -c "kubeadm init --kubernetes-version=\`kubeadm version -o short\` --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.7.0.10 --ignore-preflight-errors=all"
 docker exec k8s-master mkdir -p /root/.kube
